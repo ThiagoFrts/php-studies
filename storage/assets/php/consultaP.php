6c581a05969f9fdca0c1e9b8804bfc167a882df1
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-09-24 10:56:33 -0300

    Update consultaP.php

--- storage/assets/php/consultaP.php
+++ storage/assets/php/consultaP.php
@@ -13,30 +13,52 @@
         <h3>Selecione um fornecedor para visualizar as informações.</h3>
     </header>
 
-    <form action="">
-        <select name="" id="">
-            <option value="">
-                <?php
+    <?php include_once('conexao.php'); ?>
 
-                include_once('conexao.php');
+    <form action="consultaP.php" method="get">
+        <label for="">Fornecedor</label>
+        <select name="fornecedor" id="" required>
+            <option selected disabled value="">Selecione um fornecedor</option>
+            <?php
+            $consultaFornecedor = mysqli_query(
+                $conexao,
+                "SELECT id_fornecedor, nome 
+                 FROM fornecedor "
+            );
 
-                $consulta = mysqli_query(
-                    $conexao,
-                    "SELECT fornecedor.id_fornecedor Fornecedor, fornecedor.nome Nome
-                 FROM fornecedor
-                 WHERE fornecedor = '$'
-            
-            
-                "
-                );
-
-
-
-                ?>
-            </option>
+            while ($linha = mysqli_fetch_array($consultaFornecedor)) {
+                $selecionado = (isset($_GET['fornecedor']) && $_GET['fornecedor'] == $linha['id_fornecedor']) ? 'selected' : '';
+                echo "<option value='" . $linha['id_fornecedor'] . "' $selecionado>" . $linha['nome'] . "</option>";
+            }
+            ?>
         </select>
+        <button type="submit">Buscar</button>
     </form>
 
+    <?php
+    if (!empty($_GET['fornecedor'])) {
+        $idfornecedor = $_GET['fornecedor'];
+
+        $consulta = mysqli_query($conexao, "SELECT produto.nome, produto.qtd_estoque, produto.preco
+            FROM produto
+            WHERE id_fornecedor = '$idfornecedor'");
+
+        if (mysqli_num_rows($consulta) > 0) {
+            echo "<table border = '1'>
+                 <tr><th>Produto</th><th>Qtd Estoque</th><th>Preço</th></tr>";
+            while ($p = mysqli_fetch_assoc($consulta)) {
+                echo "<tr>
+                        <td>" . $p['nome'] . "</td>
+                        <td>" . $p['qtd_estoque'] . "</td>
+                        <td>R$ " . number_format($p['preco'], 2, ',', '.') . "</td>
+                      </tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "<h3>Nenhum produto cadastrado para este fornecedor.</h3>";
+        }
+    }
+    ?>
 
     <a href="../../index.html"> <button>Voltar para a aba inicial</button></a>
 </body>
